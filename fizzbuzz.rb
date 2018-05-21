93f8378b61298ed2a65afc99f8db86e7fc9eaf06
Peter Kim <peterckim96@gmail.com>
2018-05-21 18:22:24 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,8 @@
 def fizzbuzz(number)
-  if number % 3 = 0
-    puts "Fizz"
+  if number % 15 = 0
+    puts "FizzBuzz"
   elsif number % 5 = 0
     puts "Buzz"
-  else
+  elsif number % 3 = 0
+    puts "Fizz"
 end
